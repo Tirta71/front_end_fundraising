6f4bd05ddf210117b0cf916f01e86a67a672e8aa
tirta71 <tirta4132@gmail.com>
2024-05-20 19:15:43 +0700

    fix

--- src/components/forms/CauseDetailsForm.tsx
+++ src/components/forms/CauseDetailsForm.tsx
@@ -64,7 +64,7 @@ const CauseDetailsForm = ({ id_cause }: CauseDetailsFormProps) => {
 
       const { snap_token } = response.data;
 
-      snap.pay(snap_token, {
+      snaping.pay(snap_token, {
         onSuccess: async function (result: any) {
           try {
             await axios.post(
