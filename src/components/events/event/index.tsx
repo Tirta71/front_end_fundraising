28f743f7b9b8e6b16c5c1318e14883b287f8636e
tirta71 <tirta4132@gmail.com>
2024-06-12 00:18:05 +0700

    bismilah bisa

--- src/components/events/event/index.tsx
+++ src/components/events/event/index.tsx
@@ -1,21 +1,23 @@
-import Breadcrumb from "@/components/common/Breadcrumb"
-import FooterOne from "@/layout/footers/FooterOne"
-import HeaderOne from "@/layout/headers/HeaderOne"
-import EventArea from "./EventArea"
-import Brand from "@/components/common/Brand"
+import Breadcrumb from "@/components/common/Breadcrumb";
+import FooterOne from "@/layout/footers/FooterOne";
+import HeaderOne from "@/layout/headers/HeaderOne";
+import Brand from "@/components/common/Brand";
 
 const Event = () => {
-   return (
-      <>
-         <HeaderOne style_1={false} style_2={false} />
-         <main>
-            <Breadcrumb page_title="Our Latest Event" page_list="Event" style={false} />
-            <EventArea />
-            <Brand style={true} />
-         </main>
-         <FooterOne />
-      </>
-   )
-}
+  return (
+    <>
+      <HeaderOne style_1={false} style_2={false} />
+      <main>
+        <Breadcrumb
+          page_title="Our Latest Event"
+          page_list="Event"
+          style={false}
+        />
+        <Brand style={true} />
+      </main>
+      <FooterOne />
+    </>
+  );
+};
 
 export default Event;
